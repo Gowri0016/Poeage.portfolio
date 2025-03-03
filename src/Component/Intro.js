b665bcf5abc4b83b20952bde60b1b509d70ad594
Gowri <gowri@Gowris-MacBook-Air.local>
2025-03-04 01:10:14 +0530

    poeage

--- src/Component/Intro.js
+++ src/Component/Intro.js
@@ -11,7 +11,7 @@ export default function Intro() {
   return (
     <div>
 
-    <div className="text-1.5xl md:h-80 mx-4 md:mx-64 mt-10">
+    <div className="text-1.5xl h-auto md:h-80 mx-4 md:mx-64 mt-10">
       <h1 className="text-3xl md:text-8xl font-extralight hover:text-blue-700 mt-6">Welcome to Portfolio</h1>
       <p className="mt-6 md:mt-16 text-sm md:text-base">
         Hi, I hope you're doing well. I am <span className="text-blue-600">Gowri Shankar</span> and I’m a web developer
@@ -19,12 +19,13 @@ export default function Intro() {
         create engaging visuals, user-friendly websites, etc. to stand out and connect with their audience. I'd love the opportunity
         to collaborate with you on your upcoming projects. If you have any design needs or ideas you'd like to explore, feel free to reach out.
       </p>
-    </div>
-
-    <div className="text-1.5xl md:h-80 mx-4 md:mx-64 mt-10">
-      <h1 className=" flex justify-center relative bottom-5 font-extralight text-3xl md:text-4xl hover:text-blue-700 mt-6">Why Choose Us</h1>
+      </div>
     
-      <div className="relative z-10 flex flex-wrap justify-center gap-8 md:gap-14 px-4">
+
+    <div className="text-1.5xl h-auto md:h-80 mx-4 md:mx-64 mt-10">
+      <h1 className=" flex justify-center relative bottom-5 font-extralight text-3xl md:text-4xl hover:text-blue-700 mt-6">Why Choose Us</h1>
+    </div>
+      <div className="relative z-10 bottom-48 flex flex-wrap justify-center gap-8 md:gap-14 px-4">
 
               <div className="w-full sm:w-64 md:w-72 lg:w-1/4 text-black bg-white font-semibold text-center p-8 rounded-lg shadow-lg transition duration-300 transform hover:scale-105">
               <img className="w-16 h-16 mx-auto mb-4" src={Exp} alt={Exp}></img>
@@ -64,9 +65,7 @@ export default function Intro() {
               </div>
 </div>
 
-    </div>
-
-    
 
     </div>
+
   )}
